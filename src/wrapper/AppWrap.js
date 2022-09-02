d0b69c1b2911911d9ff7d68d2cbf852e65503c73
KnightTaa <74761059+KnightTaa@users.noreply.github.com>
2022-09-02 17:02:06 +0530

    First Commit 1

---
+++ src/wrapper/AppWrap.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavigationDots, SocialMedia } from '../components'
+
+const AppWrap = (Components, idName, classNames) => function HOC() {
+  return (
+    <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia/>
+
+        <div className='app__wrapper app__flex'>
+            <Components/>
+        </div>
+        <NavigationDots active={idName}/>
+    </div>
+  )
+}
+
+export default AppWrap
